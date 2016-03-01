2f81e73f4ade8061c71c3186143b574eff029aa3
Christian Müller <no@way.click>
2016-03-01 16:44:23 +0100

    better messaging

--- run.py
+++ run.py
@@ -14,7 +14,8 @@ with open(args['urls']) as fileHandler:
     urls = map(lambda line: line.strip(), fileHandler.readlines())
 
 # download images
-progress = 0
+attempt = 0
+fetched = 0
 for url in urls:
     # note: it would be fatal to extract the file name from the URL and use it as a local file name due to
     # different URLs with the same file name! As no explicit requirement about the file name is known,
@@ -23,13 +24,16 @@ for url in urls:
     # in case if we couldn't guess (!) an extension
     if (len(extension) != 3): extension = "dat"
     fileName = args['output'] + "/" + str(uuid.uuid1()) + "." + extension
-    print ("[" + str(progress) + "/" + str(len(urls)) + "] Storing file...", end = " ")
+    print ("[" + str(attempt) + "/" + str(len(urls)) + "] Fetching...", end = " ")
     try:
         response = urllib2.urlopen(url)
         f = open(fileName, 'wb')
         f.write(response.read())
         f.close()
         print ("Stored " + url + " as " + fileName + " ...")
+        fetched = fetched + 1
     except urllib2.URLError, e:
         print ("Failed with " + url + ": " + str(e))
-    progress = progress + 1
+    attempt = attempt + 1
+    
+print ("Successfully fetched {0} files from {1}".format(fetched, attempt))
