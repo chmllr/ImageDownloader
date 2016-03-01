846198eb775a79c4bab221cca6993bbae5403cb4
Christian Müller <no@way.click>
2016-03-01 16:53:19 +0100

    IO error handling added

--- run.py
+++ run.py
@@ -2,12 +2,21 @@ from __future__ import print_function
 import argparse
 import urllib2
 import uuid
+import sys
+import os
 
 # parse all command line arguments
 parser = argparse.ArgumentParser(description='Downloads images stored in a text file as a flat list of URLs.')
 parser.add_argument('--urls', help='path to the text file with URLs', required=True)
 parser.add_argument('--output', help='directory where downloaded images should be stored', required=True)
 args = vars(parser.parse_args());
+
+outputDirectory = args['output']
+
+# are we allowed to run?
+if (not os.path.isdir(outputDirectory) or not os.path.exists(outputDirectory)):
+    print (outputDirectory + " does not exists or is not a directory")
+    sys.exit(1)
 
 # read and trim all URLs
 with open(args['urls']) as fileHandler:
@@ -23,14 +32,17 @@ for url in urls:
     extension = url.split(".")[-1]
     # in case if we couldn't guess (!) an extension
     if (len(extension) != 3): extension = "dat"
-    fileName = args['output'] + "/" + str(uuid.uuid1()) + "." + extension
+    fileName = outputDirectory + "/" + str(uuid.uuid1()) + "." + extension
+    # inform user about what's going on
     print ("[" + str(attempt) + "/" + str(len(urls)) + "] Fetching...", end = " ")
+    sys.stdout.flush()
     try:
+        # try to fetch the URL and stored in case of a success
         response = urllib2.urlopen(url)
         f = open(fileName, 'wb')
         f.write(response.read())
         f.close()
-        print ("Stored " + url + " as " + fileName + " ...")
+        print ("Stored " + url + " as " + fileName)
         fetched = fetched + 1
     except urllib2.URLError, e:
         print ("Failed with " + url + ": " + str(e))
