37f11285492634b9ce341f38b34f4a0b01382812
Christian Müller <no@way.click>
2016-03-01 15:52:50 +0100

    arguments parsed

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
-print "hello world"
+import argparse
+
+parser = argparse.ArgumentParser(description='Downloads images stored in a text file as a flat list of URLs.')
+parser.add_argument('--urls', help='path to the text file with URLs', required=True)
+parser.add_argument('--output', help='directory where downloaded images should be stored', required=True)
+
+args = vars(parser.parse_args());
