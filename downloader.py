771cd5d9e30a24e077a32e91a5b4d3a38ccfa559
Christian Müller <chmllr@me.com>
2016-03-02 14:59:11 +0100

    code split into a separate class to allow unit testing

---
+++ downloader.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+import urllib2
+import uuid
+import sys
+
+class Downloader():
+
+    def __init__(self, fileName, outputDirectory):
+        self.outputDirectory = outputDirectory        
+        # read and trim all URLs
+        with open(fileName) as fileHandler:
+            self.urls = map(lambda line: line.strip(), fileHandler.readlines())
+
+    def downloadSync(self):
+        # download images
+        attempts = 0
+        fetched = 0
+        for url in self.urls:
+            attempts = attempts + 1
+            # note: it would be fatal to extract the file name from the URL and use it as a local file name due to
+            # different URLs with the same file name! As no explicit requirement about the file name is known,
+            # we generate a GUID string for every local file name.
+            extension = url.split(".")[-1]
+            # in case if we couldn't guess (!) an extension
+            if (len(extension) == 0 or len(extension) > 4): extension = "dat"
+            fileName = self.outputDirectory + "/" + str(uuid.uuid1()) + "." + extension
+            # inform user about what's going on
+            print("[" + str(attempts) + "/" + str(len(self.urls)) + "] Fetching...", end = " ")
+            sys.stdout.flush()
+            try:
+                # try to fetch the URL and store it in case of a success
+                response = urllib2.urlopen(url)
+                f = open(fileName, 'wb')
+                f.write(response.read())
+                f.close()
+                print("Stored " + url + " as " + fileName)
+                fetched = fetched + 1
+            except urllib2.URLError, e:
+                print("Failed with " + url + ": " + str(e))
+        return (fetched, attempts)
--- run.py
+++ run.py
@@ -1,9 +1,7 @@
-from __future__ import print_function
 import argparse
-import urllib2
-import uuid
 import sys
 import os
+import downloader
 
 # parse all command line arguments
 parser = argparse.ArgumentParser(description='Downloads images stored in a text file as a flat list of URLs.')
@@ -18,35 +16,9 @@ if (not os.path.isdir(outputDirectory) or not os.path.exists(outputDirectory)):
     print(outputDirectory + " does not exists or is not a directory")
     sys.exit(1)
 
-# read and trim all URLs
-with open(args['urls']) as fileHandler:
-    urls = map(lambda line: line.strip(), fileHandler.readlines())
+instance = downloader.Downloader(args['urls'], outputDirectory)
 
-# download images
-attempt = 0
-fetched = 0
-for url in urls:
-    # note: it would be fatal to extract the file name from the URL and use it as a local file name due to
-    # different URLs with the same file name! As no explicit requirement about the file name is known,
-    # we generate a GUID string for every local file name.
-    extension = url.split(".")[-1]
-    # in case if we couldn't guess (!) an extension
-    if (len(extension) == 0 or len(extension) > 4): extension = "dat"
-    fileName = outputDirectory + "/" + str(uuid.uuid1()) + "." + extension
-    # inform user about what's going on
-    print("[" + str(attempt) + "/" + str(len(urls)) + "] Fetching...", end = " ")
-    sys.stdout.flush()
-    try:
-        # try to fetch the URL and store it in case of a success
-        response = urllib2.urlopen(url)
-        f = open(fileName, 'wb')
-        f.write(response.read())
-        f.close()
-        print("Stored " + url + " as " + fileName)
-        fetched = fetched + 1
-    except urllib2.URLError, e:
-        print("Failed with " + url + ": " + str(e))
-    attempt = attempt + 1
+(fetched, attempts) = instance.downloadSync()
 
-print()
-print("Successfully fetched {0} files from {1}".format(fetched, attempt))
+print
+print("Successfully fetched {0} files from {1}".format(fetched, attempts))
